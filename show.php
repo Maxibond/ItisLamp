765d2a8fe865afc75ffe7c7277fd886172aeaafe
Maxibond <jeymontor@yandex.ru>
2015-04-18 18:03:26 +0300

    food store

---
+++ show.php
@@ -0,0 +1,34 @@
+<!DOCTYPE>
+<html>
+<head>
+ <title>Интернет-магазин "Патриотъ" </title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+<body>
+<?php
+    echo '<table border>
+    <tr>
+    	<td>Номер</td>
+    	<td>Получатель</td>
+    	<td>Товар</td>
+    	<td>Кол-во</td>
+    	<td>Комментарий от заказчика</td>
+    </tr>';
+	$row =0;
+	if (( $fp = fopen('file.csv','r'))!==FALSE){
+		while (($data = fgetcsv($fp, 1000, ','))!==FALSE){
+			$num = count($data);
+			$row++;
+			echo '<tr>';
+			echo '<td align="middle">'.$row.'</td>';
+			for($c=0;$c < $num; $c++){
+				echo '<td align="middle">'.$data[$c]."</td>";
+			}
+			echo '</tr>';
+		}
+		fclose($fp);
+	}
+	echo '</tabe>';
+?>
+</body>
+</html>
